58b694f07b252a83941b945243307ae225a5ed88
Jefferson Griebel <jg.awsprod@gmail.com>
2025-11-21 21:50:06 -0500

    initial commit

---
+++ burger.js
@@ -0,0 +1,14 @@
+"use strict";
+const getElement = selector => document.querySelector(selector);
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    getElement("#add_order").addEventListener("click", () => {
+        
+    }); 
+
+    getElement("#clear_order").addEventListener("click", () => {
+        
+    });
+    
+}); 
